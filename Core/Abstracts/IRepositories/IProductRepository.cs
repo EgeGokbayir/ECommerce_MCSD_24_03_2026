ea2b1a95e6e5f2ff2711446bfc530c196ac7e360
EgeGokbayir <egegokbayir925@gmail.com>
2026-03-25 14:40:31 +0300

    update 0.3 25.03.2026

---
+++ Core/Abstracts/IRepositories/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using Core.Concretes.Entities;
+using Utils.Generics;
+
+namespace Core.Abstracts.IRepositories
+{
+    public interface IProductRepository : IRepository<Product> { }
+    public interface ICategoryRepository : IRepository<Category> { }
+    public interface ISubcategoryRepository : IRepository<Subcategory> { }
+    public interface IBrandRepository : IRepository<Brand> { }
+    public interface IProductImageRepository : IRepository<ProductImage> { }
+    public interface IProductAttributeRepository : IRepository<ProductAttribute> { }
+    public interface IProductReviewRepository : IRepository<ProductReview> { }
+    public interface ICartRepository : IRepository<Cart> { }
+    public interface ICartItemRepository : IRepository<CartItem> { }
+}
---
+++ Utils/Generics/IRepository.cs
@@ -0,0 +1,199 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using Utils.Responses;
+
+namespace Utils.Generics
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IResult> CreateAsync(T entity);
+        Task<IResult> CreateManyAsync(IEnumerable<T> entities);
+        Task<IResult<IEnumerable<T>>> FindManyAsync(Expression<Func<T, bool>>? expression = null, params string[] includes);
+        Task<IResult<T>> FindByIdAsync(object id);
+        Task<IResult<T>> FindFirstAsync(Expression<Func<T, bool>>? expression = null);
+
+        Task<IResult> UpdateAsync(T entity);
+        Task<IResult> UpdateManyAsync(IEnumerable<T> entities);
+
+        Task<IResult> DeleteAsync(object id);
+        Task<IResult> DeleteAsync(T entity);
+        Task<IResult> DeleteManyAsync(IEnumerable<T> entites);
+        Task<IResult> DeleteManyAsync(Expression<Func<T, bool>>? expression = null);
+
+        Task<IResult<int>> CountAsync(Expression<Func<T, bool>>? expression = null);
+        Task<IResult<bool>> AnyAsync(Expression<Func<T, bool>>? expression = null);
+    }
+
+    public abstract class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly DbContext _db;
+        protected readonly DbSet<T> _table;
+
+        protected Repository(DbContext db)
+        {
+            _db = db;
+            _table = _db.Set<T>();
+        }
+
+        public async Task<IResult<bool>> AnyAsync(Expression<Func<T, bool>>? expression = null)
+        {
+            try
+            {
+                bool any = expression != null ? await _table.AnyAsync(expression) : await _table.AnyAsync();
+                return Result<bool>.Success(any);
+            }
+            catch (Exception ex)
+            {
+                return Result<bool>.Failure([ex.Message], 500);
+            }
+        }
+
+        public async Task<IResult<int>> CountAsync(Expression<Func<T, bool>>? expression = null)
+        {
+            try
+            {
+                int count = expression != null ? await _table.CountAsync(expression) : await _table.CountAsync();
+                return Result<int>.Success(count);
+            }
+            catch (Exception ex)
+            {
+                return Result<int>.Failure([ex.Message], 500);
+            }
+        }
+
+        public async Task<IResult> CreateAsync(T entity)
+        {
+            try
+            {
+                await _table.AddAsync(entity);
+                return Result.Success(201);
+            }
+            catch (Exception ex)
+            {
+                return Result.Failure([ex.Message], 500);
+            }
+        }
+
+        public async Task<IResult> CreateManyAsync(IEnumerable<T> entities)
+        {
+            try
+            {
+                await _table.AddRangeAsync(entities);
+                return Result.Success(201);
+            }
+            catch (Exception ex)
+            {
+                return Result.Failure([ex.Message], 500);
+            }
+        }
+
+        public async Task<IResult> DeleteAsync(object id)
+        {
+            var result = await FindByIdAsync(id);
+
+            if (result.IsSuccess)
+                return await DeleteAsync(result.Data);
+            return result;
+        }
+
+        public async Task<IResult> DeleteAsync(T entity)
+        {
+            try
+            {
+                await Task.Run(() => _table.Remove(entity));
+                return Result.Success(204);
+            }
+            catch (Exception ex)
+            {
+                return Result.Failure(ex.Message, 500);
+            }
+        }
+
+        public async Task<IResult> DeleteManyAsync(IEnumerable<T> entites)
+        {
+            try
+            {
+                await Task.Run(() => _table.RemoveRange(entites));
+                return Result.Success(204);
+            }
+            catch (Exception ex)
+            {
+                return Result.Failure(ex.Message, 500);
+            }
+        }
+
+        public async Task<IResult> DeleteManyAsync(Expression<Func<T, bool>>? expression = null)
+        {
+            var entities = expression == null ? _table : _table.Where(expression);
+            if (entities == null)
+            {
+                return Result.Failure("Entities not found", 404);
+            }
+            return await DeleteManyAsync(entities);
+        }
+
+        public async Task<IResult<T>> FindByIdAsync(object id)
+        {
+            var entity = await _table.FindAsync(id);
+
+            if (entity == null)
+            {
+                return Result<T>.Failure(["Entity Not found!"], 404);
+            }
+            return Result<T>.Success(entity);
+        }
+
+        public async Task<IResult<T>> FindFirstAsync(Expression<Func<T, bool>>? expression = null)
+        {
+            var entity = expression == null ? await _table.FirstOrDefaultAsync() : await _table.FirstOrDefaultAsync(expression);
+            if (entity == null)
+            {
+                return Result<T>.Failure(["Entity Not found!"], 404);
+            }
+            return Result<T>.Success(entity);
+        }
+
+        public async Task<IResult<IEnumerable<T>>> FindManyAsync(Expression<Func<T, bool>>? expression = null, params string[] includes)
+        {
+            var entities = expression == null ? _table : _table.Where(expression);
+
+            if (entities == null)
+            {
+                return Result<IEnumerable<T>>.Failure(["Entities Not found!"], 404);
+            }
+
+            foreach (var include in includes)
+            {
+                entities = entities.Include(include);
+            }
+
+            return Result<IEnumerable<T>>.Success(await entities.ToListAsync());
+        }
+
+        public async Task<IResult> UpdateAsync(T entity)
+        {
+            try
+            {
+                await Task.Run(() => _table.Update(entity));
+                return Result.Success();
+            }
+            catch (Exception ex)
+            {
+                return Result.Failure(ex.Message, 500);
+            }
+        }
+
+        public async Task<IResult> UpdateManyAsync(IEnumerable<T> entities)
+        {
+            try
+            {
+                await Task.Run(() => _table.UpdateRange(entities));
+                return Result.Success();
+            }
+            catch (Exception ex)
+            {
+                return Result.Failure(ex.Message, 500);
+            }
+        }
+    }
+}
